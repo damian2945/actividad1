6d2f768ff5d7f060cea0ed1ed10097af9404d0b0
damian2945 <145505947+damian2945@users.noreply.github.com>
2024-09-17 14:43:05 -0600

    cambio

---
+++ parcial 2/JS/hola.js
@@ -0,0 +1,30 @@
+
+
+
+
+/*let a = parseInt(prompt("Ingrese un número"));
+let b = parseInt(prompt("Ingrese otro número"));
+let resultado = a + b;
+alert("La suma de los números es: " + resultado);
+*/
+let a = parseInt(prompt("Ingrese un número"));
+let b = parseInt(prompt("Ingrese otro número"));
+let resultado = a - b;
+alert("La resta de los números es: " - resultado);
+
+
+
+
+/*
+ACT 3
+let a = parseInt(prompt("Ingrese un número"));
+let b = parseInt(prompt("Ingrese otro número"));
+
+if (a > b) {
+    alert("El número " + a + " es mayor que " + b);
+} else if (a < b) {
+    alert("El número " + b + " es mayor que " + a);
+} else {
+    alert("no, introducir letras");
+}*/
+
