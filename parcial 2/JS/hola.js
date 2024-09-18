019c83951a3f3d9b5eba2e234d1a199d8d90abca
damian2945 <145505947+damian2945@users.noreply.github.com>
2024-09-17 19:00:58 -0600

    cambio

--- parcial 2/JS/hola.js
+++ parcial 2/JS/hola.js
@@ -1,31 +1,26 @@
+// Pedimos al usuario que ingrese dos números
+let num1 = parseFloat(prompt("Ingrese el primer número:"));
+let num2 = parseFloat(prompt("Ingrese el segundo número:"));
 
 
+let suma = num1 + num2;
+let resta = num1 - num2;
+let multiplicacion = num1 * num2;
+let division = num1 / num2;
 
 
-/*let a = parseInt(prompt("Ingrese un número"));
-let b = parseInt(prompt("Ingrese otro número"));
-let resultado = a + b;
-alert("La suma de los números es: " + resultado);
-*/
-let a = parseInt(prompt("Ingrese un número"));
-let b = parseInt(prompt("Ingrese otro número"));
-let resultado = a - b;
-alert("La resta de los números es: " - resultado);
+let mayorQue = num1 > num2;
+let menorQue = num1 < num2;
+let igualQue = num1 === num2;
 
+let mensaje = "Los resultados son:\n" +
+              "Suma: " + suma + "\n" +
+              "Resta: " + resta + "\n" +
+              "Multiplicación: " + multiplicacion + "\n" +
+              "División: " + division + "\n" +
+              "¿El primer número es mayor que el segundo? " + mayorQue + "\n" +
+              "¿El primer número es menor que el segundo? " + menorQue + "\n" +
+              "¿Los números son iguales? " + igualQue;
 
-let array = [1, 2, 3, 4, 5];    
-
-
-/*
-ACT 3
-let a = parseInt(prompt("Ingrese un número"));
-let b = parseInt(prompt("Ingrese otro número"));
-
-if (a > b) {
-    alert("El número " + a + " es mayor que " + b);
-} else if (a < b) {
-    alert("El número " + b + " es mayor que " + a);
-} else {
-    alert("no, introducir letras");
-}*/
+alert(mensaje);
 
