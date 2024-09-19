21a16fa7828c09cfd1f3a86bc6d89df96b132fdc
damian2945 <145505947+damian2945@users.noreply.github.com>
2024-09-19 16:44:14 -0600

    camvios

---
+++ parcial 2/app.js
@@ -0,0 +1,6 @@
+const menu = document.querySelector('.navbar-nav');
+
+menu.innerHTML += `<li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="estructura.html">programacion</a>
+                  </li>
+                  <li class="n`;
