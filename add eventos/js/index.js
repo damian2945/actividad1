c10b53d2d474bf24af176c7894280e5eefb88251
damian2945 <145505947+damian2945@users.noreply.github.com>
2024-09-24 18:27:56 -0600

    cambio

---
+++ add eventos/js/index.js
@@ -0,0 +1,5 @@
+const boton = document.getElementById('boton');
+boton.addEventListener('click', (e) => {
+    alert('Hola mundo');
+
+});
--- parcial 2/actividad login/js/app.js
+++
@@ -1,7 +0,0 @@
-const form = document.getElementById('form');
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-
-
-    window.location.href = 'ucol.html';
-});
