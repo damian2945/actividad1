7888c949fc9a6d5f5db75adeb8d34953a998165d
damian2945 <145505947+damian2945@users.noreply.github.com>
2024-09-24 22:42:00 -0600

    cambios

--- add eventos/js/index.js
+++ add eventos/js/index.js
@@ -1,5 +1,21 @@
 const boton = document.getElementById('boton');
-boton.addEventListener('click', (e) => {
-    alert('Hola mundo');
+boton.addEventListener('click', agregarCarta);
 
-});
+    function agregarCarta() {
+        const nombre = document.getElementById('nombre').value;
+        const lugar= document.getElementById('lugar').value;
+        const descripcion = document.getElementById('descripcion').value;
+      
+        const nuevaCarta = document.createElement('div');
+        nuevaCarta.classList.add('carta');
+        nuevaCarta.innerHTML = `<div class="card" style="width: 18rem;">
+        <div class="card-body">
+       <h5 class="card-title">${nombre}</h5>
+        <p class="card-text">${lugar}</p>
+        <a href="#" class="btn btn-primary">${descripcion}</a>
+         </div>
+          </div>`;
+        const contenedor = document.getElementById('contenedor');
+        contenedor.appendChild(nuevaCarta);
+
+}
