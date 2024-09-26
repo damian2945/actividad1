ce6f11d57b0638c8eb04e12def84341d427ff685
damian2945 <145505947+damian2945@users.noreply.github.com>
2024-09-26 13:31:56 -0600

    cambios

--- add eventos/js/index.js
+++ add eventos/js/index.js
@@ -12,7 +12,7 @@ boton.addEventListener('click', agregarCarta);
         <div class="card-body">
        <h5 class="card-title">${nombre}</h5>
         <p class="card-text">${lugar}</p>
-        <p class="card-text">${descripcion}}</p>
+        <p class="card-text">${descripcion}</p>
          </div>
           </div>`;
         const contenedor = document.getElementById('contenedor');
