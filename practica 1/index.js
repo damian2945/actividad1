50963a3a7fbd168b16ee5f8cadbd8c4f84c9a4ff
damian2945 <145505947+damian2945@users.noreply.github.com>
2024-10-10 01:04:13 -0600

    cambios

--- practica 1/index.js
+++ practica 1/index.js
@@ -8,13 +8,20 @@ const boss = document.getElementById('boss');
             console.log(dress);
             dress.forEach(dress => {
                 console.log(ReadableStreamDefaultReader);
-                boss.innerHTML += `<div class="card" style="width: 18rem;">
-  <img src="${dress.image}" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">${dress.title}</h5>
-    <p class="card-text">$${dress.price}</p>
-    
-  </div>
-</div>`;
+                boss.innerHTML += `<div class="col-md-4">
+                <div class="card mb-4 box-shadow">
+                  <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" alt="Thumbnail [100%x225]" src="${dress.image}" data-holder-rendered="true" style="height: 20rem; width: 95%; display: block;">
+                  <div class="card-body">
+                    <p class="card-text">$${dress.price}</p>
+                    <p class="card-text">${dress.id}</p>
+                    <div class="d-flex justify-content-between align-items-center">
+                      <div class="btn-group">
+                      </div>
+                      <small class="text-muted">${dress.category}</small>
+                    </div>
+                  </div>
+                </div>
+              </div>`;
             });
         });
+
